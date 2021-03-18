3e69b92a9605b1d3abd64c072b2c98c3cf4d7508
NFickett <71030361+NFickett@users.noreply.github.com>
2021-03-18 10:31:35 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# SoftwareCapstone
+
+# Team: Mikala, Nathan, Michael, Thomas
+
+# Project: Beakon
+# COVID-19 Tracking Web App
+
+# Description:
+
+# Sources: 
+# Tailwind CSS Tutorial: https://www.youtube.com/playlist?list=PL4cUxeGkcC9gpXORlEHjc5bgnIi5HEGhw
+#   - Used for setting up Tailwind on Visual Studio Code
+
+# Homepage Template: https://github.com/tailwindtoolbox/Admin-Template
+#   - Copyright 2018-2019 Astrava.Solutions Ltd. Code released under the MIT license.
+
+# Login Template: https://github.com/davidgrzyb/tailwind-login-template
+#   - Copyright 2020 David Grzyb. Licensed under the Apache License, Version 2.0 (the "License").
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+//router
+const authRoutes = require("./routes/authRoutes");
+const app = express();
+
+//midleware
+app.use(express.static('public'));
+
+
+
+//veiw engine
+app.set('view engine','ejs');
+
+// database connection
+
+const dbURI =
+  "mongodb+srv://Nathan:TrlDs2mXMnPbjEAP@cluster0.a6xht.mongodb.net/node-auth";
+  mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+
+//routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/test", (req, res) => res.render("test"));
+app.get('/faq',(req,res) => res.render("faq"))
+app.get('/announcements',(req,res) => res.render("announcements"))
+app.use(authRoutes);
