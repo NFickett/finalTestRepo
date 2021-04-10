0347864929454827d3ffab1f59bc412cabbacb1a
Nathan Fickett <nathanefickett@lewisu.edu>
2021-04-10 00:09:33 -0500

    update from mikalas branch

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var path = require('path')
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const {requireAuth, checkUser} = require ('./middleware/authMiddleware')
 //router
 const authRoutes = require("./routes/authRoutes");
 const app = express();
@@ -10,6 +11,8 @@ var port = process.env.PORT ||3000;
 
 //midleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -18,19 +21,20 @@ app.set('view engine','ejs');
 
 // database connection
 
-const dbURI =
-  "mongodb+srv://Nathan:TrlDs2mXMnPbjEAP@cluster0.a6xht.mongodb.net/node-auth";
-  mongoose
-  .connect(dbURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then((result) => app.listen(port))
-  .catch((err) => console.log(err));
+//const dbURI =
+//  "mongodb+srv://Nathan:TrlDs2mXMnPbjEAP@cluster0.a6xht.mongodb.net/node-auth";
+//  mongoose
+//  .connect(dbURI, {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+//    useCreateIndex: true,
+//  })
+//  .then((result) => app.listen(port))
+//  .catch((err) => console.log(err));
 
-
+app.listen(port)
 //routes
+app.get('*', checkUser);
 app.get("/", (req, res) => res.render("index"));
 app.get("/test", (req, res) => res.render("test"));
 app.get('/faq',(req,res) => res.render("faq"))
